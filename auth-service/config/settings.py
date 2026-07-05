397af902870ee9f6aef9dfd091317ef308506540
BeamlakF <beamlakfekadu562@gmail.com>
2026-07-05 22:13:53 +0300

    feat: token ansd otp service done-changes in settings.py too

--- auth-service/config/settings.py
+++ auth-service/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework_simplejwt.token_blacklist',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
