79863b80921d7decff4c838f1dd8425a06a18edd
TsiOn <tshimelis23@gmail.com>
2026-07-29 19:42:20 +0300

    added the folder structure of analytics service

--- auth-service/config/settings.py
+++ auth-service/config/settings.py
@@ -16,7 +16,33 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SPECTACULAR_SETTINGS = {
+    "TITLE": "A2SV Auth Service API",
+    "DESCRIPTION": "Authentication and Authorization Service for the A2SV Learning Platform.",
+    "VERSION": "1.0.0",
 
+    "SERVE_INCLUDE_SCHEMA": False,
+
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "persistAuthorization": True,
+    },
+    "SECURITY": [
+        {
+            "BearerAuth": [],
+        },
+    ],
+
+    "COMPONENTS": {
+        "securitySchemes": {
+            "BearerAuth": {
+                "type": "http",
+                "scheme": "bearer",
+                "bearerFormat": "JWT",
+            }
+        }
+    },
+}
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -39,7 +65,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework',
+    'drf_spectacular',
+    'drf_spectacular_sidecar',
+    
 ]
 
 MIDDLEWARE = [
@@ -119,6 +149,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "adapters.inbound.rest.authentication.JWTAuthentication",
+    ],
+}
 # config/settings.py
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- payment-service/config/urls.py
+++ payment-service/config/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("api/v1/", include("adapters.inbound.rest.urls")),
+    path("api/schema", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui")
 ]
