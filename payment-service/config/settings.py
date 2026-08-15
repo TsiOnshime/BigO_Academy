1fdf89d86d2fb2b3b73a6568c06729e90d1a4635
TsiOn <tshimelis23@gmail.com>
2026-08-15 07:17:01 +0300

    chore: added frontend url to cors_allowed_origins

--- payment-service/config/settings.py
+++ payment-service/config/settings.py
@@ -44,7 +44,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5174",
     "http://localhost:5175",
     "http://127.0.0.1:5175",
-    "https://bigo-academy.vercel.app",
+    "https://big-o-academy-frontend.vercel.app/",
 ]
 CORS_ALLOW_CREDENTIALS = True
  
